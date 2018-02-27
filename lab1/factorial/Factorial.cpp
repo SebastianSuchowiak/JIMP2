83624b701a974b210b44656eb4dd1df54f8d1689
Sebastian Suchowiak <suchseba@borg.kis.agh.edu.pl>
2018-02-27 18:04:54 +0100

    commit po pierwszych zajęciach

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,22 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+
+    int result = 1;
+
+    if (value >= 13 || value <= -13) {
+        return 0;
+    }
+
+    if (value > 0) {
+        for (int i = 1; i <= value; i++) {
+            result *= i;
+        }
+    } else if (value < 0) {
+        for (int i = -1; i >= value; i--) {
+            result *= i;
+        }
+    }
+
+    return result;
 }
